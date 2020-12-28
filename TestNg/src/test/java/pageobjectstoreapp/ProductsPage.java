95027dcae96c6a384f2abf5b5967380d599b7d6d
ajinkyagh <ajinkyagh@gmail.com>
2020-12-28 15:58:45 +0530

    Avoids code duplication by using page object model.

--- TestNg/src/test/java/pageobjectstoreapp/ProductsPage.java
+++ TestNg/src/test/java/pageobjectstoreapp/ProductsPage.java
@@ -4,8 +4,14 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidBy;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class ProductsPage {
     private final Utilities utilities;
@@ -18,7 +24,18 @@ public class ProductsPage {
         this.driver = driver;
     }
 
+    @AndroidFindBy(id = "com.androidsample.generalstore:id/productName")
+    public List<WebElement> ProductName;
 
+
+    @AndroidFindBy(id="com.androidsample.generalstore:id/productAddCart")
+    public List<WebElement> AddToCartClk;
+
+    @AndroidFindBy(id = "com.androidsample.generalstore:id/appbar_btn_cart")
+    public WebElement FinalCart;
+
+    @AndroidFindBy(id = "com.androidsample.generalstore:id/productName")
+    public WebElement LastText;
 
     public void ProductAdd() throws InterruptedException {
       //        Actual Test Case
@@ -28,21 +45,27 @@ public class ProductsPage {
 
 //        Click Add To cart button test
 
-        int count =driver.findElementsById("com.androidsample.generalstore:id/productName").size();//Returns size based on total products on the screen not all
+        int count =ProductName.size();
+        //driver.findElementsById("com.androidsample.generalstore:id/productName").size();//Returns size based on total products on the screen not all
         System.out.println(count);
 
         for(int i=0;i<count;i++){
-            text=driver.findElementsById("com.androidsample.generalstore:id/productName").get(i).getText() ;
+            text=ProductName.get(i).getText();
+            //driver.findElementsById("com.androidsample.generalstore:id/productName").get(i).getText() ;
+
             if (text.equalsIgnoreCase("Jordan 6 Rings")){
-                driver.findElementsById("com.androidsample.generalstore:id/productAddCart").get(i).click();
+                AddToCartClk.get(i).click();
+                //driver.findElementsById("com.androidsample.generalstore:id/productAddCart").get(i).click();
                 break;
             }
         }
-        driver.findElementById("com.androidsample.generalstore:id/appbar_btn_cart").click();
+        FinalCart.click();
+        //driver.findElementById("com.androidsample.generalstore:id/appbar_btn_cart").click();
         Thread.sleep(5000);
 
 //       Validation of the items selected on page2 with page3
-        String lastpagetext=driver.findElementById("com.androidsample.generalstore:id/productName").getText();
+        String lastpagetext=LastText.getText();
+        //driver.findElementById("com.androidsample.generalstore:id/productName").getText();
 
         if (lastpagetext.equals("Jordan 6 Rings")) {
             System.out.println("Case Passed");
